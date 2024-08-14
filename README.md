6583e636b2b1cddc0fec4a45e5273fad694b3046
Tryd0g0lik <work80@mail.ru>
2024-08-14 19:53:27 +0700

    [master]: pause

--- README.md
+++ README.md
@@ -1,5 +1,55 @@
+13.08
 Настроена рабочая среда, в то числе eslit и Husky дл автоматической проверки.\
 2024-08-14 замусь бозой данных и логкой.
 
-
+14.08
+1. Создал модильный модуль для логирования  при работе нв сервера. (можно и на фронте его использоваить). \
+2000 строк (уровень можно регулировать) файл удаялется и создается новый. \
+```test
+16:25:10:37 ==> [server -> ERROR]: 'Users' Something that wrong!
+     ERR-MEASSAGE: ошибка синтаксиса (примерное положение: "DEAFULT")
+16:25:10:37 ==> [server]: "Emails" returning FALSE.
+16:25:10:37 ==> [server]: END
+16:27:38:15 ==> [server]: START
+16:27:38:16 ==> [server]: Start the "Client" connection.
+16:27:38:48 ==> [server -> ERROR1]: 'createDBTable' Something that wrong!
+     ERR-MEASSAGE: база данных "london" уже существует
+16:27:38:48 ==> [server -> Client2]: Start the  "Client" connection.
+16:27:38:73 ==> [server]: "Emails" was created .
+16:27:38:34 ==> [server]: "Users" was created .
+16:27:38:34 ==> [server]: The end the "Client" connection.
+16:27:38:34 ==> [server]: END
+16:28:34:4 ==> [server]: START
+16:28:34:6 ==> [server]: Start the "Client" connection.
+16:28:34:36 ==> [server -> ERROR1]: 'createDBTable' Something that wrong!
+     ERR-MEASSAGE: база данных "london" уже существует
+16:28:34:36 ==> [server -> Client2]: Start the  "Client" connection.
+16:28:34:63 ==> [server]: "Emails" was created .
+16:28:34:63 ==> [server]: "Users" was created .
+16:28:34:63 ==> [server]: The end the "Client" connection.
+16:28:34:63 ==> [server]: END
+16:29:58:51 ==> [server]: START
+16:29:58:52 ==> [server]: Start the "Client" connection.
+16:29:58:83 ==> [server -> ERROR1]: 'createDBTable' Something that wrong!
+     ERR-MEASSAGE: база данных "london" уже существует
+16:29:58:83 ==> [server -> Client2]: Start the  "Client" connection.
+16:29:58:9 ==> [server]: "Emails" was created .
+16:29:58:9 ==> [server]: "Users" was created .
+16:29:58:9 ==> [server]: The end the "Client" connection.
+16:29:58:10 ==> [server]: END
+19:28:25:2 ==> [server -> listen]: Server starte to listen the PORT 7070
+```
+2. запуск проекта вызывает логику ответственную за создание самой бзы данных и создание двух таблиц в базе данных.  \
+Таблица Users связана с таблицей  Emails. \
 void prymaryInstalation();
+
+3. роутер запросов на сервере  изначально писал на 'require("http");' \
+Данная библиотека слабая для большого проекта и закомментировал.
+Сделал набросок роутера на основе frameworke require("express")
+
+на 15/08 надо писать sql для работы с REST API и совместить с роутерем запросов.
+
+Очень много времени отнимается работа с debug через `console.loge` и самописными `log-ирование`. В слепую фактически. 
+
+:-) Может поскажете способ логирования сервеной лигики помимо бзценных console.log и лигов?
+
--- src/backend/src/Server/index.ts
+++ src/backend/src/Server/index.ts
@@ -1,48 +1,74 @@
 const http_ = require('http');
-const pg_ = require('pg');
-const log = require('../logs/index');
-const APP_SERVER_PORT = (process.env.APP_SERVER_PORT as string | unknown) || 7070;
 
-const fortune = (ctx: typeof http_.request,
+const express = require('express');
+// const pg_ = require('pg');
+const log = require('./logs/index');
+const APP_SERVER_PORT = (process.env.APP_SERVER_PORT as string | unknown) || 7070;
+const APP_POSTGRES_HOST_ = (process.env.APP_POSTGRES_HOST as string | unknown) || 'localhost';
+const app = express();
+
+// const fortune = (ctx: typeof http_.request,
+//   body: string = '',
+//   status: number = 200): void => {
+//   void new Promise<void>((resolve, reject) => {
+//     log(`[server -> fortune]: STATUS ${ctx.response.status}
+//       BODY ${ctx.response.bpdy}`);
+//     ctx.writeHead(status, { 'Content-Type': 'application/json' });
+//     ctx.end(body);
+//     resolve();
+//   });
+// };
+const fortune = (res: typeof http_.request,
   body: string = '',
-  status: number = 200): void => {
+  statusCode: number = 200): void => {
   void new Promise<void>((resolve, reject) => {
-    log(`[server -> fortune]: STATUS ${ctx.response.status}
-      BODY ${ctx.response.bpdy}`);
-    ctx.writeHead(status, { 'Content-Type': 'application/json' });
-    ctx.end(body);
+    log(`[server -> fortune]: STATUS ${res.response.status}
+      BODY ${res.response.bpdy}`);
+    res.status(statusCode).json(body);
     resolve();
   });
 };
-const clientsId = new String(/clients\/[0-9]?/);
-log(`[server -> clientsId]: ${clientsId as string} `);
-async function router(req: typeof http_.request): Promise<void> {
-  console.log(`[server -> router]: typeof req <=> ${(typeof req).includes(typeof http_.request)} `);
-  if ((typeof req).includes(typeof http_.request)) {
-    log(`[server -> router]: ${typeof req} `);
-    req.writeHead(500, { 'Content-Type': 'application/json' });
-    const body = JSON.stringify({ data: false });
-    fortune(req, body, 500);
-    log('[server -> router]: Start send data');
-  }
-  if ((req.url).imvludes('/api/v1/clients/')) {
-    log('[server -> router]: get all clients');
-  } else if ((req.url).imvludes()) {
-    log('[server -> router]: clients/id');
-  } else if ((clientsId).includes(req.url as string)) {
-    // '/api/v1/patch/clients/id'
-    log('[server -> router]: clients/id');
-  } else {
-    log('[server -> router]: 404 code');
-    const body = JSON.stringify({ data: false });
-    fortune(req, body, 404);
-  }
-}
-const server = http_.createServer(async (req: unknown, res: unknown) => {
-  void router(req);
-});
+// const clientsId = new String(/clients\/[0-9]?/);
+// log(`[server -> clientsId]: ${clientsId as string} `);
+// async function router(req: typeof http_.request): Promise<void> {
+//   console.log(`[server -> router]: typeof req <=> ${(typeof req).includes(typeof http_.request)} `);
+//   if ((typeof req).includes(typeof http_.request)) {
+//     log(`[server -> router]: ${typeof req} `);
+//     req.writeHead(500, { 'Content-Type': 'application/json' });
+//     const body = JSON.stringify({ data: false });
+//     fortune(req, body, 500);
+//     log('[server -> router]: Start send data');
+//   }
+//   if ((req.url).imvludes('/api/v1/clients/')) {
+//     log('[server -> router]: get all clients');
+//   } else if ((req.url).imvludes()) {
+//     log('[server -> router]: clients/id');
+//   } else if ((clientsId).includes(req.url as string)) {
+//     // '/api/v1/patch/clients/id'
+//     log('[server -> router]: clients/id');
+//   } else {
+//     log('[server -> router]: 404 code');
+//     const body = JSON.stringify({ data: false });
+//     fortune(req, body, 404);
+//   }
+// }
+// const server = http_.createServer(async (req: unknown, res: unknown) => {
+//   // void router(req);
 
-server.listen(APP_SERVER_PORT, () => {
+// });
+
+app.get('/api/v1/clients/', (req: unknown, res: unknown) => {
+  log('[server -> router]: clients');
+  const body = JSON.stringify({ data: true });
+  fortune(res, body);
+});
+app.get('/api/v1/clients/:id/', () => { });
+app.post('/api/v1/clients/add/', () => { });
+app.post('/api/v1/clients/add/', () => { });
+app.put('/api/v1/clients/:id/', () => { });
+app.delete('/api/v1/clients/:id/', () => { });
+
+app.listen(APP_SERVER_PORT, APP_POSTGRES_HOST_, () => {
   log(`[server -> listen]: Server starte to listen the PORT ${APP_SERVER_PORT as string}`);
   console.log(`[server]: Server starte to listen the PORT ${APP_SERVER_PORT as string}`);
 });
