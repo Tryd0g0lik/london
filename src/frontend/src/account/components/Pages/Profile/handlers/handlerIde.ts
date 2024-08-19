9156a1c2be1d169111c675481bc7e84f14ce7cd3
Tryd0g0lik <work80@mail.ru>
2024-08-19 09:30:43 +0700

    [master]: Here 'Pronfile/handlers' was added

---
+++ src/frontend/src/account/components/Pages/Profile/handlers/handlerIde.ts
@@ -0,0 +1,39 @@
+// import React from 'react';
+
+export async function handlerIdeFC(): Promise<boolean> {
+
+  const switchs = document.querySelector('.profil.form');
+  if (switchs === null) {
+    return false;
+  }
+  let index = 0;
+  (switchs as HTMLDivElement).addEventListener('click', async (event: MouseEvent): Promise<boolean> => {
+
+    // const target = event.target;
+
+    if ((event.type) && ((event.type).toLowerCase() !== 'click')) {
+      return false;
+    }
+
+    let mouseTarget = event.target;
+    if (((mouseTarget as HTMLInputElement).tagName).toLowerCase() !== 'input') {
+      return false;
+    }
+    const oldClassName = (mouseTarget as HTMLInputElement).className;
+
+    if (index === 0) {
+      (mouseTarget as HTMLInputElement).className =
+        (mouseTarget as HTMLInputElement).className.replace(' ', ' active ');
+      index++;
+    } else if (oldClassName.includes('active') && (index === 1)) {
+
+      oldClassName.replace('activve', '');
+      (mouseTarget as HTMLInputElement).className =
+        (mouseTarget as HTMLInputElement).className.replace('active ', '');
+      index -= 1;
+      // убрали класс ACTIVE - выключили редактирование.
+    }
+    return true;
+  });
+  return true;
+}
---
+++ src/frontend/src/account/components/Pages/Profile/handlers/installInfo.ts
@@ -0,0 +1,38 @@
+
+import { get } from '@Services/fetches';
+import { getCookie } from '@Services/coockieSessionId';
+
+export async function infoLoader(): Promise<boolean> {
+  const sessionId = getCookie('sessionId');
+  if (sessionId === undefined) {
+    return false;
+  }
+  interface ResultType {
+    id: number;
+    firstName: string;
+    lastName: string;
+    name: string
+  }
+  const result = await get(JSON.stringify({}), `/api/v1/clients/${sessionId}`) as ResultType;
+  if ((typeof result) === 'boolean') {
+    return false;
+  }
+  const profile = document.querySelector('.profil');
+  const profileALL = document.querySelectorAll('.profil > div');
+  if (profileALL === null && profile === null) {
+    return false;
+  };
+
+
+
+  const index = String(result.id as number);
+  const name = result.firstName as string;
+  const lastName = result.lastName as string;
+  const d = profileALL[0].querySelector('div') as HTMLElement;
+  const d2 = profileALL[1].querySelector('div') as HTMLElement;
+  (profile as HTMLDivElement).dataset.index = index;
+  d.innerHTML = name;
+  d2.innerHTML = lastName;
+
+  return true;
+}
