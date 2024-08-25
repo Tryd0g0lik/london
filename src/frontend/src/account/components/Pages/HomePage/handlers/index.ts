22af2ab54bbb225da45e519cf8e218cd9bad513a
Tryd0g0lik <work80@mail.ru>
2024-08-25 17:01:00 +0700

    [master]: Here a friend's servise the START work

--- src/frontend/src/account/components/Pages/HomePage/handlers/index.ts
+++ src/frontend/src/account/components/Pages/HomePage/handlers/index.ts
@@ -1,10 +1,8 @@
-import React, { KeyboardEvent } from 'react'; // { useState, SetStateAction }
 import { get } from '@Services/fetches';
 import { getCookie, checkCookieExists } from '@Services/coockieSessionId';
 import { messageForUser } from '@Services/messengerForm';
-import { createDivLableContainer } from '@Services/fields';
 
-import { OneLine, Ads } from '@Interfaces';
+import { Ads } from '@Interfaces';
 
 export async function loaderProfilesAll(): Promise<boolean> {
   const divHtml = document.querySelector('.list-profiles');
@@ -17,7 +15,7 @@ export async function loaderProfilesAll(): Promise<boolean> {
     sessionId = getCookie('sessionId') as string;
   }
 
-  const pathnameStr = `/api/v1/clients/profiles/all/${sessionId}`;
+  const pathnameStr = `/api/v1/profiles/all/${sessionId}`;
   const body_ = JSON.stringify({});
   const result = await get(body_, pathnameStr) as unknown as Ads;
   if (typeof result === 'boolean' || !(result.positions)) {
--- src/frontend/src/account/components/Pages/HomePage/index.tsx
+++ src/frontend/src/account/components/Pages/HomePage/index.tsx
@@ -1,9 +1,11 @@
 import React, { JSX, useEffect } from 'react'
 import { NavFC } from '@Components/NavOpPages'
 import { checkerCookieKey } from '@Services/coockieSessionId';
+import { loaderProfilesAll } from './handlers';
 export function HomePageFC(): JSX.Element {
   useEffect(() => {
     checkerCookieKey();
+    loaderProfilesAll()
 
   });
   return (<>
