6306030d759633624add22c79b09c7bd950d7ce6
Tryd0g0lik <work80@mail.ru>
2024-08-27 12:44:17 +0700

    [master]: pause

--- src/frontend/src/account/components/Pages/Profile/index.tsx
+++ src/frontend/src/account/components/Pages/Profile/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { NavFC } from '@Components/NavOpPages';
 import { checkerCookieKey, checkCookieExists } from '@Services/coockieSessionId';
 import { infoLoader } from './handlers/installInfo';
