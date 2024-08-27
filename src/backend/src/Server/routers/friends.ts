462e79961e7c196322d04b0ec69b7cf526de6f1d
Tryd0g0lik <work80@mail.ru>
2024-08-27 18:08:14 +0700

    [master]: clearing 2 now

--- src/backend/src/Server/routers/friends.ts
+++ src/backend/src/Server/routers/friends.ts
@@ -1,26 +1,16 @@
 const { Router, Request, Response, NextFunction } = require('express');
-const { propsForClient, sendNotFound, addNewFriendSQL } = require('./handlers');
-// const { Client } = require('pg');
+const { propsForClient } = require('./handlers');
 const {
-  loaderAllProfilesSQL, selectOneParamSQL
+  loaderAllProfilesSQL
 } = require('../sql-functions/index');
 const { clients } = require('../clients');
-// const { getCookie } = require('../getCookies');
-// const { checkerDubleEmails } = require('../validators');
-const log = require('../logs/index');
 const router = Router();
 export async function routerFriends(routers: typeof router): Promise<typeof router> {
   routers.get('/api/v1/profiles/all/:sessionId', async (req: typeof Request, res: typeof Response, next: typeof NextFunction) => {
     const sessionId = req.params.sessionId;
-    // const regex = /^\d+$/;
-    // if (!regex.test(sessionId as string)) {
-    //   return;
-    // }
-    await log(`[server -> router -> friends]: GET Friend ALL sessionId =>: ${sessionId}`);
     /* --------- Below, we is get the data from all profiles --------- */
     let respArr = await clients(loaderAllProfilesSQL);
 
-    await log(`[server -> router -> friends]: GET Friend ALL Counter ${respArr.rows.length}`);
     if ((respArr.rows.length > 0) && (sessionId.length > 5)) {
       respArr = (respArr.rows).filter((item: typeof propsForClient) => item.session_id !== sessionId);
     } else if (respArr.rows.length === 0) {
@@ -28,7 +18,6 @@ export async function routerFriends(routers: typeof router): Promise<typeof rout
     } else {
       respArr = respArr.rows;
     }
-    await log(`[server -> router -> friends]: GET Friend ALL Before sending. Counter2 ${respArr.length}`);
     res.status(200).json({ message: 'OK', profiles: respArr });
   });
   return routers;
