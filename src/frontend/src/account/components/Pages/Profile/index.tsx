1e21067ff84e7dcdc38c30e8e24cf0c59225f079
Tryd0g0lik <work80@mail.ru>
2024-08-27 12:43:43 +0700

    [master]: pause

--- src/frontend/src/account/components/Pages/Profile/index.tsx
+++ src/frontend/src/account/components/Pages/Profile/index.tsx
@@ -3,29 +3,29 @@ import { NavFC } from '@Components/NavOpPages';
 import { checkerCookieKey, checkCookieExists } from '@Services/coockieSessionId';
 import { infoLoader } from './handlers/installInfo';
 import { handlerIdeFC } from './handlers/handlerIde'
-const basisHtml = <>
-  <div data-namex='firstName' className='profilename profile-FirstName'></div>
-  <div data-namex='lastName' className='profilename profile-LastName'></div>
-</> as React.ReactNode;
-const divHtml = <>
-  <div data-namex='firstName' className='profilename profile-FirstName'>
-    <span>First name</span>
-    <input type="checkbox" className="switchs toggle toggle-xs" defaultChecked />
-    <div></div>
-    <div className='switchs-rewrite'></div>
-  </div>
-  <div data-namex='lastName' className='profilename profile-LastName'>
-    <span>Last name</span>
-    <input type="checkbox" className="switchs  toggle toggle-xs" defaultChecked />
-    <div></div>
-    <div className='switchs-rewrite'></div>
-  </div>
-</> as React.ReactNode;
+// const basisHtml = <>
+//   <div data-namex='firstName' className='profilename profile-FirstName'></div>
+//   <div data-namex='lastName' className='profilename profile-LastName'></div>
+// </> as React.ReactNode;
+// const divHtml = <>
+//   <div data-namex='firstName' className='profilename profile-FirstName'>
+//     <span>First name</span>
+//     <input type="checkbox" className="switchs toggle toggle-xs" defaultChecked />
+//     <div></div>
+//     <div className='switchs-rewrite'></div>
+//   </div>
+//   <div data-namex='lastName' className='profilename profile-LastName'>
+//     <span>Last name</span>
+//     <input type="checkbox" className="switchs  toggle toggle-xs" defaultChecked />
+//     <div></div>
+//     <div className='switchs-rewrite'></div>
+//   </div>
+// </> as React.ReactNode;
 export function ProfileFC(): React.JSX.Element {
   // const [status, setStatus] = useState<React.ReactNode>(basisHtml);
 
 
-  const status = checkCookieExists('sessionId');
+  // const status = checkCookieExists('sessionId');
   useEffect((): () => void => {
 
     const funAsync = async (): Promise<boolean> => {
